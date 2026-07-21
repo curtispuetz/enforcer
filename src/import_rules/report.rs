95b2449c6d9bc8cad6572e3270ef2c3917f93a48
Curtis Puetz <36495179+curtispuetz@users.noreply.github.com>
2026-07-21 11:08:31 +0300

    Update report.rs

--- src/import_rules/report.rs
+++ src/import_rules/report.rs
@@ -21,27 +21,4 @@ fn _print_failures(passed: usize, violations: Vec<Violation>) {
             println!("  {import}");
         }
     }
-    println!();
-    _print_rules();
-}
-
-fn _print_rules() {
-    println!(
-        "For failures, they are somehow breaking the file structure rules (which \
-        checked for both src/ and test/ files): "
-    );
-    println!("  1. a file can only import from its own directory or sub directories");
-    println!(
-        "  2. Exception to rule 1 is that files can import from c directories \
-        if the prefix of that c directory path is in the files prefix"
-    );
-    println!(
-        "  3. Rules 1 and 2 also apply within a c directory as if that c \
-        directory were its own root directory project."
-    );
-    println!(
-        "  4. The rules don't apply inside 's' (static data), 't' (types), \
-        or 'ext_traits' directories, nor when any other file imports from \
-        these directories."
-    );
 }
